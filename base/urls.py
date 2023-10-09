68fff3c9dbe02b5bc046c5717541254f759959c2
Gilad Weinberger <giladwe21@gmail.com>
2023-10-09 13:16:02 +0300

    .

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 
-app_name = 'base'
-
 urlpatterns = [
     path('', views.Home, name='home'),
     path('topics/', views.all_topics, name='all_topics'),
