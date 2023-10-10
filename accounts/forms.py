f1644602cde950e82f88c8c264955d139337e828
Gilad Weinberger <giladwe21@gmail.com>
2023-10-10 11:25:57 +0300

    .

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,9 +3,6 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser  
 
 class RegistrationForm(UserCreationForm):
-    email = forms.EmailField()
-    profile_image = forms.ImageField() 
-
     class Meta:
         model = CustomUser 
         fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2', 'profile_image']
