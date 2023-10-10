24ae828ce50adfc1e1e2fef4ed88e66120b15b50
Gilad Weinberger <giladwe21@gmail.com>
2023-10-10 13:45:46 +0300

    .

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,6 +3,8 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import User  
 
 class MyUserCreationForm(UserCreationForm):
+    email = forms.EmailField()
+    
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'avatar', 'password1', 'password2']
