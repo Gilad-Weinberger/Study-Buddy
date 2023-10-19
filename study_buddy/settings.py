56e881dfa5c0d5deee0e21c877dd6b7d6b898a2a
Gilad Weinberger <giladwe21@gmail.com>
2023-10-19 15:35:27 +0300

    connect mongoDB 1

--- study_buddy/settings.py
+++ study_buddy/settings.py
@@ -76,10 +76,17 @@ WSGI_APPLICATION = 'study_buddy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'Study_Buddy_db',
     }
 }
 
