42a19a348b9bcc39807f14bf2c825306ff3aadfc
Gilad Weinberger <giladwe21@gmail.com>
2023-11-06 09:55:50 +0200

    return to normal

--- study_buddy/settings.py
+++ study_buddy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-em^^d(em%hs3y4_&762%6p(0^vm7nz!*@hh9ldr%7++k!y1_i4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.noe.sh', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -119,8 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
