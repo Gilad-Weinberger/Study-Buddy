26aff897f00c70fbaa8c742a301fa421b639f5d2
Gilad Weinberger <giladwe21@gmail.com>
2023-12-12 15:16:16 +0200

    debug vercel 3

--- study_buddy/settings.py
+++ study_buddy/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'study_buddy.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
+        'NAME': str('db.sqlite3'),
     }
 }
 
