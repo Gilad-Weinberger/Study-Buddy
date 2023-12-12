b5d64e5c0ea191e070bbc59c011b78a7bb15b8de
Gilad Weinberger <giladwe21@gmail.com>
2023-12-12 15:19:52 +0200

    try to add host

--- study_buddy/settings.py
+++ study_buddy/settings.py
@@ -79,7 +79,8 @@ WSGI_APPLICATION = 'study_buddy.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str('db.sqlite3'),
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
+        'HOST': 'https://gilad-study-buddy.vercel.app/',
     }
 }
 
